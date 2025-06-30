4eb91564823bdb804621f3e4b6e8de50082b950e
LuisMAC2022 <LuisMAC2022@users.noreply.github.com>
2025-06-30 23:13:01 +0000

    deploy: ed497b51a3b8bb74c27870908ea3edb29118882b

--- wasm_game/target/wasm32-unknown-unknown/release/deps/wasm_game.d
+++ wasm_game/target/wasm32-unknown-unknown/release/deps/wasm_game.d
@@ -1,5 +1,6 @@
-/home/runner/work/Achtli/Achtli/wasm_game/target/wasm32-unknown-unknown/release/deps/wasm_game.d: src/lib.rs
+/home/runner/work/Achtli/Achtli/wasm_game/target/wasm32-unknown-unknown/release/deps/wasm_game.d: src/lib.rs src/growth.rs
 
-/home/runner/work/Achtli/Achtli/wasm_game/target/wasm32-unknown-unknown/release/deps/wasm_game.wasm: src/lib.rs
+/home/runner/work/Achtli/Achtli/wasm_game/target/wasm32-unknown-unknown/release/deps/wasm_game.wasm: src/lib.rs src/growth.rs
 
 src/lib.rs:
+src/growth.rs:
